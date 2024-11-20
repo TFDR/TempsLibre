8b0b1dda63f703ea874114625bf93bbaa9e56c12
drouet-djimy <drouet.djimy@gmail.com>
2024-11-20 11:01:54 +0100

    initial commit

---
+++ Backend/src/main/java/fr/diginamic/tempsLibre/TempsLibreApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.tempsLibre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempsLibreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TempsLibreApplication.class, args);
+	}
+
+}
