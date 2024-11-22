fa5b2f20cbabdcad65906f353858389cb0d5aec4
Ramzi <ramzibenmansour.pro@gmail.com>
2024-11-22 16:29:21 +0100

    init data function

--- Backend/src/main/java/fr/diginamic/tempsLibre/TempsLibreApplication.java
+++ Backend/src/main/java/fr/diginamic/tempsLibre/TempsLibreApplication.java
@@ -2,12 +2,21 @@ package fr.diginamic.tempsLibre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class TempsLibreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TempsLibreApplication.class, args);
-	}
+		ApplicationContext context = SpringApplication.run(TempsLibreApplication.class, args);
 
+		// à decommenter pour initialiser les données
+        /*
+        DepartmentRepository departmentRepository = context.getBean(DepartmentRepository.class);
+        EmployeeRepository employeeRepository = context.getBean(EmployeeRepository.class);
+
+        InitDataLoader initDataLoader = new InitDataLoader(departmentRepository, employeeRepository);
+        initDataLoader.initData();
+        */
+	}
 }
